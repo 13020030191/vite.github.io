fca270c41fcf14e042a7026167da5498e39f2d8e
13020030191 <48125485+13020030191@users.noreply.github.com>
2021-09-11 15:54:15 +0800

    Delete static directory

--- static/index.21b83b9d.js
+++
@@ -1,2 +0,0 @@
-import{r as a,c as e,w as n,o,a as r,b as s,T as t}from"./vendor.c9c32ea2.js";const u={};u.render=function(u,l){const c=a("router-view");return o(),e(c,null,{default:n((({Component:a})=>[r(t,null,{default:n((()=>[(o(),e(s(a)))])),_:2},1024)])),_:1})};export{u as default};
-//# sourceMappingURL=index.21b83b9d.js.map
