7251b373d18fa71f74583965f053536f81ff5b26
13020030191 <48125485+13020030191@users.noreply.github.com>
2021-09-11 13:24:18 +0800

    Delete static directory

--- static/index.e5fa55e6.js
+++
@@ -1,2 +0,0 @@
-import{r as a,c as e,w as n,o as s,a as t,b as o,T as r}from"./index.04f0560a.js";const u={render:function(u,l){const f=a("router-view");return s(),e(f,null,{default:n((({Component:a})=>[t(r,null,{default:n((()=>[(s(),e(o(a)))])),_:2},1024)])),_:1})}};export{u as default};
-//# sourceMappingURL=index.e5fa55e6.js.map
